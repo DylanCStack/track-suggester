5065a52f70dc20e28704e7da7ae033bab642d6e4
Dylan Stackhouse <stackhouse.dylan@gmail.com>
2017-01-13 14:27:33 -0800

    add intro to programming(night) as a possible result for users

--- js/scripts.js
+++ js/scripts.js
@@ -104,21 +104,24 @@ $(document).ready(function(){
       night +=.5;
     }
 
-    if(java>php && java>ruby && java>css && java>cnet) {
+    if(java>php && java>ruby && java>css && java>cnet && java>night) {
       $(".firstResult").text("Java/Android");
       $(".explination1").text(" has broad appeal for both large and small enterprises. It is a high demand language with a range of uses. It is specifically vital for Android developers.");
-    } else if (php>java && php>ruby && php>css && php>cnet) {
+    } else if (php>java && php>ruby && php>css && php>cnet && php>night) {
       $(".firstResult").text("Php/Drupal");
       $(".explination1").text(" is used in content-heavy, low interactivity websites. It is used widely from small buisness sites to large government operations. It is very useful for building websites for specific buisness or organizations.");
-    } else if (ruby>java && ruby>php && ruby>css && ruby>cnet) {
+    } else if (ruby>java && ruby>php && ruby>css && ruby>cnet && ruby>night) {
       $(".firstResult").text("Ruby/Rails");
       $(".explination1").text(" is used for building interactive web applications, and dynamic websites. It is a very popular language and framework for small startup companies. It can be used to make a product very quickly.");
-    } else if (css>java && css>php && css>ruby && css>cnet) {
+    } else if (css>java && css>php && css>ruby && css>cnet && css>night) {
       $(".firstResult").text("CSS/Design");
       $(".explination1").text(" is the defacto track for the front-end developers wanting to attend Epicodus. You will learn how to create various website layouts, user interfaces, and proper navigation.");
-    } else if (cnet>java && cnet>php && cnet>ruby && cnet>css) {
+    } else if (cnet>java && cnet>php && cnet>ruby && cnet>css && cnet>night) {
       $(".firstResult").text("C#/.NET");
       $(".explination1").text(" is most popular for large businesses and corporations, particularly for use in creating internally used software.");
+    } else {
+      $(".firstResult").text("Intro to Programming(night)");
+      $(".explination1").text(" seems like the best choice for you because you seem unsure of your future as a developer, yet you want to come to Epicodus. Therefore you should take the nighttime intro to programming class. This will allow you to familiarize yourself with coding and give you time to make up your mind on what track you will want to take in the future. Its cheap too!");
     }
 
     $("#survey").hide();
